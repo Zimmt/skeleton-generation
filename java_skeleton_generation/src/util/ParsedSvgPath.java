16de5c066ad002d3a7dcaa5980bd74d9b750fccb
nina <nina.zimbel@web.de>
2020-01-14 14:56:32 +0100

    Implement reader that parses svg files to get coordinates of paths

---
+++ java_skeleton_generation/src/util/ParsedSvgPath.java
@@ -0,0 +1,23 @@
+package util;
+
+import javax.vecmath.Point2d;
+import java.util.List;
+
+public class ParsedSvgPath {
+
+    private String label;
+    private List<Point2d> points;
+
+    public ParsedSvgPath(String label, List<Point2d> points) {
+        this.label = label;
+        this.points = points;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<Point2d> getPoints() {
+        return points;
+    }
+}
---
+++ java_skeleton_generation/src/util/SvgReader.java
@@ -0,0 +1,120 @@
+package util;
+
+import javax.vecmath.Point2d;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SvgReader {
+
+    // TODO return double[]
+    public List<ParsedSvgPath> parseFile(String inputPath) throws IOException {
+        File file = new File(inputPath);
+        BufferedReader fileReader = new BufferedReader(new FileReader(file));
+        List<ParsedSvgPath> result = new ArrayList<>();
+
+        String line;
+        while ((line = fileReader.readLine()) != null) {
+
+            for (int i = 0; i < 5 && i < line.length(); i++) { // at position 4 the tag should be
+
+                if (line.length() < i+5) {
+                    break;
+                }
+                if (line.substring(i, i + 5).equals("<path")) {
+                    result.add(parsePath(fileReader));
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Searches inside the path section for the label and the point coordinates (tag: d)
+     */
+    private ParsedSvgPath parsePath(BufferedReader fileReader) throws IOException {
+        boolean foundD = false;
+        boolean foundLabel = false;
+        String label = "";
+        String d = "";
+
+        String line;
+        while(!foundLabel && (line = fileReader.readLine()) != null) {
+
+            for (int i = 0; i < 8 && i < line.length(); i++) { // at position 7 the tags should be
+                if (Character.isWhitespace(line.charAt(i))) {
+                    continue;
+                }
+                if (!foundD && line.charAt(i) == 'd') {
+                    d = line.substring(i+3, line.length()-1);
+                    foundD = true;
+                    if (Character.isLowerCase(line.charAt(i+3))) {
+                        System.err.println("Found relative coordinate!");
+                    }
+                    break;
+                } else if (foundD && line.length() > i+14 && line.substring(i, i + 14).equals("inkscape:label")) {
+                    label = line.substring(i+14+2, line.length()-4);
+                    foundLabel = true;
+                    break;
+                }
+            }
+        }
+
+        if (!label.isEmpty() && !d.isEmpty()) {
+            return parseD(label, d);
+        } else {
+            System.err.println("Couldn't parse path!");
+            return null;
+        }
+    }
+
+    /**
+     * Parses the points inside the d tag.
+     * @param label the name of the path
+     * @param d the string that contains the control points of the bezier curve
+     * @return a parsed svg path with the coordinates in a system with a y axis that points up
+     */
+    private ParsedSvgPath parseD(String label, String d) {
+        StringBuilder stringBuilder = new StringBuilder();
+        double firstCoordinate = 0f;
+        boolean firstCoordinateFound = false;
+        List<Point2d> points = new ArrayList<>();
+
+        for (int i = 0; i < d.length(); i++) {
+
+            if (!(d.charAt(i) == '-' || Character.isDigit(d.charAt(i)) || d.charAt(i) == '.')) {
+                if (stringBuilder.length() > 0) {
+                    if (!firstCoordinateFound) {
+                        firstCoordinate = Double.parseDouble(stringBuilder.toString());
+                        stringBuilder.setLength(0);
+                        firstCoordinateFound = true;
+                    } else {
+                        double secondCoordinate = Double.parseDouble(stringBuilder.toString());
+                        stringBuilder.setLength(0);
+                        points.add(getPoint(firstCoordinate, secondCoordinate));
+                        firstCoordinateFound = false;
+                    }
+                }
+
+            } else {
+                stringBuilder.append(d.charAt(i));
+            }
+        }
+
+        if (stringBuilder.length() > 0) {
+            if (firstCoordinateFound) {
+                double secondCoordinate = Double.parseDouble(stringBuilder.toString());
+                points.add(getPoint(firstCoordinate, secondCoordinate));
+            } else {
+                System.err.println("Point could not be parsed completely.");
+            }
+        }
+
+        return new ParsedSvgPath(label, points);
+    }
+
+    private Point2d getPoint(double firstCoordinate, double secondCoordinate) {
+        double fixedSecondCoordinate = 1000 - secondCoordinate; // the y-coordinate in svg files is switched (points from top to bottom)
+        return new Point2d(firstCoordinate, fixedSecondCoordinate);
+    }
+}
