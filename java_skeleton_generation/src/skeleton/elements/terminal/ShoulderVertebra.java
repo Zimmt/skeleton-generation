7564c65691c52a01c6de02fdbcbd793694fbc3af
nina <nina.zimbel@web.de>
2020-03-26 14:42:54 +0100

    Remove unused constructor from shoulder vertebra

--- java_skeleton_generation/src/skeleton/elements/terminal/ShoulderVertebra.java
+++ java_skeleton_generation/src/skeleton/elements/terminal/ShoulderVertebra.java
@@ -1,10 +1,7 @@
 package skeleton.elements.terminal;
 
-import skeleton.SpinePart;
 import skeleton.elements.joints.DummyJoint;
-import skeleton.elements.nonterminal.NonTerminalElement;
 import util.BoundingBox;
-import util.TransformationMatrix;
 
 import javax.vecmath.Point3f;
 import java.util.Optional;
@@ -16,13 +13,6 @@ public class ShoulderVertebra extends Vertebra {
 
     private String kind;
     DummyJoint shoulderJoint;
-
-    public ShoulderVertebra(TransformationMatrix transform, BoundingBox boundingBox, TerminalElement parent, NonTerminalElement ancestor,
-                            boolean positiveXDir, SpinePart spinePart, float jointSpinePosition, Point3f shoulderJointPosition) {
-        super(transform, boundingBox, parent, ancestor, positiveXDir, spinePart, jointSpinePosition);
-        this.kind = super.getKind();
-        this.shoulderJoint = new DummyJoint(this, shoulderJointPosition);
-    }
 
     public ShoulderVertebra(Vertebra vertebra) {
         super(vertebra.getTransform(), vertebra.getBoundingBox(), vertebra.getParent(), vertebra.getAncestor(), vertebra.getJoint());
